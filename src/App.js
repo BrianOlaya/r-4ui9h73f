be9f4ffc45c36c8302bd4ae599391382361d166e
brianOlaya <brilot777@gmail.com>
2020-05-06 17:22:29 -0600

    Repetidor realizado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state ={
+    name:""
+  };
+
+  cambiaEspacio =(event)=>{
+    this.setState({name :event.target.value})
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.cambiaEspacio}/>
+        <p className="repeater">{this.state.name}</p>
       </div>
     );
   }
